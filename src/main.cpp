212ce76476044e4c2362b7c26203c10138c6cb91
MikeTatsuo <mike_tatsuo@hotmail.com>
2021-02-06 19:29:55 -0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+#define DigitalPin 2
+#define AnalogPin A0
+
+int threshold;
+int analogValue;
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Digital Temperature");
+
+  lcd.setCursor(0, 2);
+  lcd.print("Threshold: ");
+
+  lcd.setCursor(0, 3);
+  lcd.print("Value: ");
+}
+
+void loop() {
+  threshold = digitalRead(DigitalPin);
+  analogValue = analogRead(AnalogPin);
+
+  lcd.setCursor(11, 2);
+  if (threshold == HIGH) {
+    lcd.print("Above");
+  } else {
+    lcd.print("Below");
+  }
+
+  lcd.setCursor(7,3);
+  lcd.print(analogValue);
+
+  delay(250);
+}
